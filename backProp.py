18fc43a1b2b439d46bf2aef1b4efa02a9a003b31
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-02-12 21:45:07 -0500

    Reading csv using pandas is working.

--- backProp.py
+++ backProp.py
@@ -1,3 +1,11 @@
 # Author: Jack Wotherspoon
 # Created: February 10th, 2019
 
+#import dependencies
+import pandas as pd
+import numpy as np
+
+#read data
+columns=['Refractive_Index','Sodium','Magnesium','Aluminum','Silicon','Potassium','Calcium','Barium','Iron','Glass_Type']
+data=pd.read_csv('GlassData.csv', names=columns)
+print(data)
